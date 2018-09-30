b8bfd845a9ee7cd1da85759aa4eee541fab4eae8
Declan Gallen <declangallen@gmail.com>
2018-09-30 13:16:36 -0500

    added comments to pavement.svy

--- pavement_svy.py
+++ pavement_svy.py
@@ -7,20 +7,26 @@ import math
 import random
 from itertools import compress
 
-path = 'C:/Users/Declan/Desktop/Python/Pavement'
+path = 'C:/Users/Declan/Desktop/Python/Pavement/Sample_files'
 os.chdir(path)
 cwd = os.getcwd()
 print(cwd)
 files = os.listdir(path)
 print(files)
 
-
+# list index locaiton of files with the correct extension .3L0
 T_F = list()
 for n in files:
 	x = '.3L0' in n
 	T_F.append(x)
 
+# loop through list of files with index of correct files (T_F).
+# file_list are all the files we will work with
 file_list = list(compress(files, T_F))
+
+# there may be some survey files in the list that are measuring other data
+# we only want the files that start with 'chain' in the datatable.
+# T_F will be overwritten with a list of lines of text from the files
 
 T_F = list()
 list_of_files = list()
@@ -37,6 +43,9 @@ for idx, n in enumerate(file_list):
 pavement_data = dict()
 for idx, val in enumerate(list_of_files):
 	d = list()
+	# load the metadata at the top of each file.
+	# if ':' is not in the string stop reading the lines
+	# d will be a list of meta data lines stripped from the top of each file
 	for a in val:
 		if ':' not in a:
 			pass
@@ -45,11 +54,17 @@ for idx, val in enumerate(list_of_files):
 			meta = list()
 			for v in c:
 				x = '_'.join(v.split())
-				c_b.append(x)
-			d.append(c_b)
+				meta.append(x)
+			d.append(meta)
 
-	txt = open(file_list[idx])
-
+	# read the dataframe in each survey file.
+	# convert two lists into a dictionary.
+	# meta data from above is now in dict format.
+	# the datatable in each file can be read in pandas format.
+	# the number of lines in the metadata will indicate where to start reading the table
+	# NOTE: there are 3 lines between the end of the metadata and the start of the table.
+	## this may be unstable.
+	# the datatable will be stored with the key DATA.
 	b = dict()
 	for no, x in enumerate(d):
 		i = iter(x)
@@ -59,18 +74,22 @@ for idx, val in enumerate(list_of_files):
 	
 	pavement_data[file_list[T_F[idx]]] = b
 
-
+# add survey file name as a column/key to each dataframe.
 for key, value in pavement_data.items():
 	value['Data']['Survey'] = key
 
-col = pavement_data['8880290M.3L0']['Data'].columns
+# check the column names
+pavement_data['8880290M.3L0']['Data'].columns
 
+# join all the tables into one.
 appended_data = []
 for key, value in pavement_data.items():
+	# get the data values for each dictionary.
 	dat = value['Data']
 	dat.columns = col
 	appended_data.append(dat)
 appended_data = pd.concat(appended_data)
+
 
 appended_data.to_csv('test.csv')
 
